dd45e8ab8158906390fd8ce32e8272d72df3d7fd
const-subject <const.subject@gmail.com>
2018-08-23 20:37:04 +0300

    Add empty main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+// Copyright (C) 2018 BitWhite Team <info@bitwhite.org>
+//
+// You can use this code in accordance with the GNU General Public License v3.0
+// which can be found in the LICENSE file.
+//
+// Please note that you can use the source code for your own purposes,
+// but we do not give any warranty. For more information, refer to the GPLv3.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Copyright (C) 2018 BitWhite Team <info@bitwhite.org>\n\n" +
+		"You can use this code in accordance with the GNU General Public License v3.0\n" +
+		"which can be found in the LICENSE file.\n\n" +
+		"Please note that you can use the source code for your own purposes,\n" +
+		"but we do not give any warranty. For more information, refer to the GPLv3.")
+}
