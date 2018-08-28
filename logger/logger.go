ac26aac10964d00d14a6e1d3336448e851d86d22
const-subject <const.subject@gmail.com>
2018-08-28 16:37:16 +0300

    logger: Add package

---
+++ logger/logger.go
@@ -0,0 +1,72 @@
+// Copyright (C) 2018 BitWhite Team <info@bitwhite.org>
+//
+// You can use this code in accordance with the GNU General Public License v3.0
+// which can be found in the LICENSE file.
+//
+// Please note that you can use the source code for your own purposes,
+// but we do not give any warranty. For more information, refer to the GPLv3
+
+package logger
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	styleYellow = "[103m"
+	styleGray   = "[90m"
+	styleReset  = "[0m"
+
+	logDebug = 0x00
+	logInfo  = 0x01
+	logErr   = 0x02
+)
+
+var logLevel = logDebug
+
+func Init(level string) {
+	switch level {
+	case "debug":
+		logLevel = logDebug
+	case "info":
+		logLevel = logInfo
+	case "err":
+		logLevel = logErr
+	default:
+		{
+			panic("Invalid logLevel type")
+		}
+
+	}
+}
+
+func Info(text ...interface{}) {
+	if logLevel == logErr {
+		return
+	}
+	print("info", text...)
+}
+
+func Debug(text ...interface{}) {
+	if logLevel != logDebug {
+		return
+	}
+	print("debug", text...)
+}
+
+func Err(text ...interface{}) {
+	print("error", text...)
+}
+
+func Fatal(text ...interface{}) {
+	print("fatal", text...)
+	panic(text[0])
+
+}
+
+func print(prefix interface{}, text ...interface{}) {
+
+	fmt.Print(fmt.Sprintf("%s%s%s %s%s ", styleYellow, prefix, styleGray, time.Now().Format("2006-01-02 15:04:05"), styleReset))
+	fmt.Println(text...)
+}
