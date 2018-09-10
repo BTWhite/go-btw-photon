79a55905ca862b34ea427779304c27e1cd55508d
const-subject <const.subject@gmail.com>
2018-09-10 15:40:00 +0300

    types, snapshot: Move vote.go

--- snapshot/vote.go
+++ snapshot/vote.go
@@ -6,25 +6,27 @@
 // Please note that you can use the source code for your own purposes,
 // but we do not give any warranty. For more information, refer to the GPLv3.
 
-package types
+package snapshot
 
 import (
 	"bytes"
 	"encoding/binary"
+
+	"github.com/BTWhite/go-btw-photon/types"
 )
 
 type Vote struct {
-	Voter     Hash  `json:"voter"`
-	Delegate  Hash  `json:"delegate"`
-	Timestamp int64 `json:"timestamp"`
-	Signature Hash  `json:"signature"`
+	Voter     types.Hash `json:"voter"`
+	Delegate  types.Hash `json:"delegate"`
+	Timestamp int64      `json:"timestamp"`
+	Signature types.Hash `json:"signature"`
 }
 
 func (v Vote) GetBytes() []byte {
 	buff := new(bytes.Buffer)
 
 	binary.Write(buff, binary.LittleEndian, v.Timestamp)
-	v.Delegate.WriteToBuff(buff, 64)
+	v.Delegate.WriteToBuff(buff, 0)
 
 	return buff.Bytes()
 }
